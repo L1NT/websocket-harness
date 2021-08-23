e564f9cbf1677a01bbcf4077747b2c3ed331b560
luke <ljeter@laresconsulting.com>
2021-08-23 17:26:24 -0500

    Add option to accept a (single) custom header
    Had a use-case that required a non-standard "Sec-WebSocket-Key" header.

--- websocket-harness.py
+++ websocket-harness.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-import socket, ssl
+#! /usr/bin/env python
+import re, socket, ssl
 import argparse
 # https://docs.python.org/3/library/http.server.html
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
@@ -39,16 +39,22 @@ def FuzzWebSocket(fuzz_payload):
 parser = argparse.ArgumentParser(description='Web Socket Harness: Use traditional pentest tools to assess web sockets')
 parser.add_argument('-u','--url', help='The remote WebSocket URL to target. Example: ws://127.0.0.1:8000/method-to-fuzz.', required=True)
 parser.add_argument('-p','--port', help='The port to bind to.', required=True, default=8000)
+parser.add_argument('-k','--custom_header', help='Any single custom header to include within the connection request', required=False)
 args = parser.parse_args()
+
+headers = {
+    'Connection': 'Upgrade',
+    'Sec-WebSocket-Version': '13',
+    'Upgrade': 'websocket'
+}
+if args.custom_header:
+    hdr = re.search('(.+): (.+)', args.custom_header)
+    headers[hdr.group(1)] = hdr.group(2)
 
 ws = create_connection(
     args.url,
     sslopt={'cert_reqs': ssl.CERT_NONE},
-    header={
-        # 'Connection': 'Upgrade',
-        # 'Sec-WebSocket-Version': '13',
-        # 'Upgrade': 'websocket'
-    },
+    header=headers,
     http_proxy_host="",
     http_proxy_port=8080,
     proxy_type="http")
